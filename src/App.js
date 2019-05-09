660028dadff90c406b993f7f9751689661acefa8
Victor Gu <vguadarrama@interware.com.mx>
2019-05-09 15:26:17 -0500

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, NavLink, Switch } from "react-router-dom";
+import Home from './components/Home';
+import Favorites from "./Containers/FavPage";
+import logo from './66.png';
+import pill from './pill.png';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import actions from './actions/home';
+import { AppLogo, Pill, TheHeader, TheNav, Navelement, Title, Wrapper, TheApp } from './App.style.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {  
+  render() {
+    return (
+      <Wrapper>
+        <TheApp>
+          <TheHeader>
+            <AppLogo src={logo} alt="logo" />
+            <Title>DOCTOR GIF</Title>
+          </TheHeader>
+          <Router>
+            <div>
+              <TheNav>
+                <ul>
+                  <Navelement>
+                    <NavLink to="/" style={liStyle}><Pill src={pill} alt="pill" />Home</NavLink>
+                  </Navelement>
+                  <Navelement>
+                    <NavLink to="/favorites" style={liStyle}><Pill src={pill} alt="pill" />Favorites</NavLink>
+                  </Navelement>
+                </ul>
+              </TheNav>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/favorites" component={Favorites} />
+                <Redirect to="/" />
+              </Switch>
+            </div>
+          </Router>
+        </TheApp>
+      </Wrapper>
+    );
+  }
 }
 
-export default App;
+let liStyle = {
+  color: 'white',
+  textDecoration: 'none'
+}
+
+const mapDispatchToProps = dispatch => {
+  const { loadFavoritesR } = actions;
+  return bindActionCreators({ loadFavoritesR }, dispatch);
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import configureStore from './store';
+import rootSaga from './sagas';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+};
+const store = configureStore(initialState);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.runSaga(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
